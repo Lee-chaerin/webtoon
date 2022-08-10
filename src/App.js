21dc48b056646737b5d6cb7384a9f9f4193ed2b4
Lee-chaerin <5543705@naver.coml>
2022-08-11 02:06:29 +0900

    html로 버튼 만들기

--- src/App.js
+++ src/App.js
@@ -15,6 +15,16 @@ const App = () => {
   return (
     <div>
       <div>
+        <button>월요일</button>
+        <button>화요일</button>
+        <button>수요일</button>
+        <button>목요일</button>
+        <button>금요일</button>
+        <button>토요일</button>
+        <button>일요일</button>
+      </div>
+
+      <div>
         <button onClick={onClick}>불러오기</button>
       </div>
       {data && <textarea rows={7} cols={20} value={JSON.stringify(data, null, 2)} />}
