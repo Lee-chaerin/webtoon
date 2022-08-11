491fa549307df3ee8ebc5e1d7c4dae96fabb43e9
Lee-chaerin <5543705@naver.coml>
2022-08-12 01:19:59 +0900

    요일별 api 연결

--- src/App.js
+++ src/App.js
@@ -12,16 +12,83 @@ const App = () => {
     }
   };
 
+
+
+  const AllMon = async() => {
+    try {
+      const res = await WebtoonAPIList.getAllMon();
+      setData(res);
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const AllTue = async() => {
+    try {
+      const res = await WebtoonAPIList.getAllTue();
+      setData(res);
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const AllWed = async() => {
+    try {
+      const res = await WebtoonAPIList.getAllWed();
+      setData(res);
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const AllThu = async() => {
+    try {
+      const res = await WebtoonAPIList.getAllThu();
+      setData(res);
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const AllFri = async() => {
+    try {
+      const res = await WebtoonAPIList.getAllFri();
+      setData(res);
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const AllSat = async() => {
+    try {
+      const res = await WebtoonAPIList.getAllSat();
+      setData(res);
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const AllSun = async() => {
+    try {
+      const res = await WebtoonAPIList.getAllSun();
+      setData(res);
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+
   return (
     <div>
       <div>
-        <button>월요일</button>
-        <button>화요일</button>
-        <button>수요일</button>
-        <button>목요일</button>
-        <button>금요일</button>
-        <button>토요일</button>
-        <button>일요일</button>
+        <button onClick={AllMon}>월요일</button>
+        <button onClick={AllTue}>화요일</button>
+        <button onClick={AllWed}>수요일</button>
+        <button onClick={AllThu}>목요일</button>
+        <button onClick={AllFri}>금요일</button>
+        <button onClick={AllSat}>토요일</button>
+        <button onClick={AllSun}>일요일</button>
       </div>
 
       <div>
--- src/services/webtoon.js
+++ src/services/webtoon.js
@@ -1,6 +1,15 @@
 import { axiosInstance } from '../utils/api';
 
 export const WebtoonAPIList = {
-  getAllWebtoon: async() => await axiosInstance.get('all/week')
+  getAllWebtoon: async() => await axiosInstance.get('all/week'),
+
+  getAllMon: async() => await axiosInstance.get('all/week?day=0'),
+  getAllTue: async() => await axiosInstance.get('all/week?day=1'),
+  getAllWed: async() => await axiosInstance.get('all/week?day=2'),
+  getAllThu: async() => await axiosInstance.get('all/week?day=3'),
+  getAllFri: async() => await axiosInstance.get('all/week?day=4'),
+  getAllSat: async() => await axiosInstance.get('all/week?day=5'),
+  getAllSun: async() => await axiosInstance.get('all/week?day=6'),
+
   // 채린 숙제 : 위의 함수 처럼 나머지 api들도 작업해주세요.
 }
