d2f3d0a9d9b36d1ed143d228cbc04dee770bbc82
Lee-chaerin <5543705@naver.coml>
2022-08-12 01:29:44 +0900

    필요한 api 작업

--- src/services/webtoon.js
+++ src/services/webtoon.js
@@ -1,7 +1,11 @@
 import { axiosInstance } from '../utils/api';
 
 export const WebtoonAPIList = {
-  getAllWebtoon: async() => await axiosInstance.get('all/week'),
+  getAllWebtoon: async() => await axiosInstance.get('all'),
+
+  getNaverWebtoon: async() => await axiosInstance.get('naver'),
+  getKakaoWebtoon: async() => await axiosInstance.get('kakao'),
+  getKakaopageWebtoon: async() => await axiosInstance.get('kakao-page'),
 
   getAllMon: async() => await axiosInstance.get('all/week?day=0'),
   getAllTue: async() => await axiosInstance.get('all/week?day=1'),
@@ -11,5 +15,9 @@ export const WebtoonAPIList = {
   getAllSat: async() => await axiosInstance.get('all/week?day=5'),
   getAllSun: async() => await axiosInstance.get('all/week?day=6'),
 
+  getSearchWebtoon: async() => await axiosInstance.get('search?keyword='),  //검색(뒤에 검색하는법 알아오기)
+
   // 채린 숙제 : 위의 함수 처럼 나머지 api들도 작업해주세요.
+
+  // 만든것 : 플랫폼별, 요일별, 검색 / 플랫폼별 요일, 연재중, 연재완료(3개 지원선배한테 만드는거 맞는지 물어보기)
 }
