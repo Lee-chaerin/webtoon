a6f92ad38f1081836e50e44d407875bd30386f35
gbwlxhd97 <gbwlxhd97@naver.com>
2022-08-28 20:06:53 +0900

    로그인 긴으 추가

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DetailPage from './pages/DetailPage';
 import SearchPage from './pages/SearchPage';
 import MainPage from './pages/MainPage';
-
 import Header from './components/Header';
+import LoginPage from './pages/LoginPage';
 
 
 const App = () => {
   return (
-    
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<MainPage />}  />
-        <Route path="/:id" element={<DetailPage />}  />
-        <Route path="/search" element={<SearchPage />}  />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/:id" element={<DetailPage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 //<Route path="/search" component={<SearchPage />} />
---
+++ src/models/user.js
@@ -0,0 +1,5 @@
+export const userDB = [
+  {id: "1", pw: "1"},
+  {id: "11", pw: "11"},
+  {id: "13", pw: "13"},
+]
